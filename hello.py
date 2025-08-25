8f57d560b89671c4595fe2fbb06afe57c237a335
senuth624 <senuthsilwa@gmail.com>
2025-08-25 17:48:49 +0530

    implement result on marks

--- hello.py
+++ hello.py
@@ -1,5 +1,16 @@
 name = input("enter your name: ")
 marks = int(input("enter Your marks: "))
 
-if marks > 75:
-    print(f"{name} you have A pss")
+
+if marks > 100 or marks < 0:
+    print(f"Invalid input! ")
+elif marks > 75:
+     print(f"{name}, you have A pass")
+elif marks >= 65:
+    print(f"{name}, you have B pass")
+elif marks >= 55:
+     print(f"{name}, you have C pass")
+elif marks >= 35:
+     print(f"{name}, you have S pass")
+else:
+     print(f"{name}, you are fail.")
