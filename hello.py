518d3088c03fc30ebbe581154b9f25b672662d73
senuth624 <senuthsilwa@gmail.com>
2025-08-25 17:33:02 +0530

    scholl mark script

--- hello.py
+++ hello.py
@@ -1,4 +1,5 @@
-name = int(input("Enter your age: "))
-print(f"your age are: {name}")
-age = int(input("Enter your age: "))
-print(f"your age is: {age}")
+name = input("enter your name: ")
+marks = int(input("enter Your marks: "))
+
+if marks > 75:
+    print(f"{name} you have A pss")
